48c8f277e6a464ed58031cf9d34b04378ec6110c
esajamal <esa.jamal@outlook.com>
2023-04-20 07:32:55 +0300

    9. Manage State by Reducer Hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return { ...state };
+  }
+};
 export default function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  //const [products, setProducts] = useState([]);
+  const [{ loading, products, error }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    products: [],
+    error: '',
+  });
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+      //setProducts(result.data);
     };
     fetchData();
   }, []);
@@ -15,7 +40,11 @@ export default function HomeScreen() {
       {' '}
       <h1> Featured products</h1>
       <div className="products">
-        {products &&
+        {loading ? (
+          <div>loading ...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
           products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
@@ -31,7 +60,8 @@ export default function HomeScreen() {
                 <button>Add to Cart</button>
               </div>
             </div>
-          ))}
+          ))
+        )}
       </div>
     </div>
   );
